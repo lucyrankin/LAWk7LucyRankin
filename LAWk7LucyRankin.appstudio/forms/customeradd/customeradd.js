2058d91c494c6dc65f3655f30cbac920ab96883f
lucyrankin <79422779+lucyrankin@users.noreply.github.com>
2021-03-22 22:58:36 -0500

    finished

--- LAWk7LucyRankin.appstudio/forms/customeradd/customeradd.js
+++ LAWk7LucyRankin.appstudio/forms/customeradd/customeradd.js
@@ -3,22 +3,19 @@
 > When user clicks a button, added this customer to the database: 
        Jesse Antiques, 1113 F St, Omaha, NE, 68178
 */
-btnAdd.onclick=function(){
-    let name = inptName.value
-    let street = inptStreet.value
-    let city = inptCity.value
-    let state = inptState.value
-    let zip = inptZip.value
-    let query = "INSERT INTO customer (name,street,city,state,zipcode) VALUES ('" + name + "', '" + street + "', '" + city + "', '" + state + "', '" + zip + "')"
-    alert(query)
-
-req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
-    if (req.status == 200) { //transit worked.
-        if (req.responseText == 500)    // for our server - this means the insert succeeded
-            lblAdd.textContent = "You have successfully added the customer!"
-        else
-            lblAdd.textContent = "There was a problem with adding the customer to the database."
-    } else 
-        // transit error
-        lblAdd.textContent = "Error: " + req.status
+btnCustomerAdd.onclick = function() {
+  let name = inptNewName.value
+  let address = inptNewAddress.value
+  let city = inptNewCity.value
+  let state = inptNewState.value
+  let zipcode = inptNewZip.value
+  let query = "INSERT INTO customers (`name`, `street`, `city`, `state`, `zipcode`) VALUES ('" + name + "', '" + address + "', '" + city + "', '" + state + "', '" + zipcode + "')"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  if (req.status == 200) {
+    if (req.responseText == 500)
+      lblAddConfirmation.textContent = "You have successfully added the a new customer!"
+    else
+      lblAddConfirmation.textContent = "There was a problem with adding the pet to the database."
+  } else
+    lblAddConfirmation.textContent = "Error: " + req.status
 }
